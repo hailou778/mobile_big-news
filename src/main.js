64017c693db61389547c524b1c502bb4bb13238a
chen <2365215192@qq.com>
2021-02-16 21:52:26 +0800

    项目开始

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入，可适配移动端，rem
+import 'amfe-flexible'
+// 全局导入Vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+// 使用Vant组件
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/utils/request.js
@@ -0,0 +1,13 @@
+// 引入axios
+import axios from 'axios'
+
+// 使用自定义配置新建一个 axios 实例
+var instance = axios.create({
+  // 基础路径
+  baseURL: 'http://ttapi.research.itcast.cn/',
+  // 超时处理
+  timeout: 3000
+})
+
+// 导出axios
+export default instance
